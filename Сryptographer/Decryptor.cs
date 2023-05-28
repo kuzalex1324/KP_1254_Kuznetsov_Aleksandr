db0de0f5b1c6fff4935cc550abec08136b1b9541
kuzalex1324 <sasha4000@inbox.ru>
2023-05-28 21:02:20 +0300

    5 Задание

---
+++ Сryptographer/Decryptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сryptographer
+{
+    internal class Decryptor
+    {
+        public string Decrypt(string key, string textToDecrypt)
+        {
+            string decryptedText = string.Empty;
+            DecryptContainer container = new DecryptContainer(key);
+
+            for (int i = 0; i < textToDecrypt.Length; i++)
+            {
+                decryptedText += container.processorList[i % container.processorList.Count].Process(textToDecrypt[i]);
+            }
+            return decryptedText;
+
+        }
+    }
+}
---
+++ Сryptographer/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Сryptographer
+{
+    public partial class Form1 : Form
+    {
+        List<Encryptor> encryptors = new List<Encryptor>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        char IncrementSymbolCode(char symbolCode)
+        {
+            return (char)((int)symbolCode + 1);
+        }
+        char DecrementSymbolCode(char symbolCode)
+        {
+            return (char)((int)symbolCode - 1);
+        }
+        char ZeroSymbolCode(char symbolCode)
+        {
+            return symbolCode;
+        }
+
+        private void BTNAddCommand_Click(object sender, EventArgs e)
+        {
+            LBSelectedCommands.Items.Add(LBAvailableComands.SelectedItem.ToString());
+        }
+
+        private void BTNDeleteCommand_Click(object sender, EventArgs e)
+        {
+            LBSelectedCommands.Items.Remove(LBSelectedCommands.SelectedItem);
+        }
+
+        private void BTNUnShifr_Click(object sender, EventArgs e)
+        {
+            Decryptor decryptor = new Decryptor();
+            TBDecryptedText.Text = decryptor.Decrypt(TBKey.Text, TBCiphertext.Text);
+        }
+
+        private void BTNShifr_Click(object sender, EventArgs e)
+        {
+            encryptors.Clear();
+            TBKey.Text = string.Empty;
+            TBCiphertext.Text = string.Empty;
+            foreach (string functionName in LBSelectedCommands.Items)
+            {
+                switch (functionName)
+                {
+                    case "Increment (+1)":
+                        encryptors.Add(new Encryptor(IncrementSymbolCode, "Increment", "I"));
+                        TBKey.Text += "I";
+                        break;
+                    case "Decrement (-1)":
+                        encryptors.Add(new Encryptor(DecrementSymbolCode, "Decrement", "D"));
+                        TBKey.Text += "D";
+                        break;
+                    case "Zero (0)":
+                        encryptors.Add(new Encryptor(ZeroSymbolCode, "Zero", "Z"));
+                        TBKey.Text += "Z";
+                        break;
+                }
+            }
+            for (int i = 0; i < TBStartText.Text.Length; i++)
+            {
+                TBCiphertext.Text += encryptors[i % encryptors.Count].Encrypt(TBStartText.Text[i]);
+            }
+        }
+
+        private void BTNUp_Click(object sender, EventArgs e)
+        {
+            string command = LBSelectedCommands.Items[LBSelectedCommands.SelectedIndex - 1].ToString();
+            LBSelectedCommands.Items[LBSelectedCommands.SelectedIndex - 1] = LBSelectedCommands.Items[LBSelectedCommands.SelectedIndex];
+            LBSelectedCommands.Items[LBSelectedCommands.SelectedIndex] = command;
+            LBSelectedCommands.SelectedIndex -= 1;
+        }
+
+        private void BTNDown_Click(object sender, EventArgs e)
+        {
+            if (LBSelectedCommands.SelectedIndex != LBSelectedCommands.Items.Count - 1)
+            {
+                string command = LBSelectedCommands.Items[LBSelectedCommands.SelectedIndex + 1].ToString();
+                LBSelectedCommands.Items[LBSelectedCommands.SelectedIndex + 1] = LBSelectedCommands.Items[LBSelectedCommands.SelectedIndex];
+                LBSelectedCommands.Items[LBSelectedCommands.SelectedIndex] = command;
+                LBSelectedCommands.SelectedIndex += 1;
+            }
+
+        }
+    }
+}
